8466376805d22939dbd09b8756770562bf98de99
root <root@vmi1735294.contaboserver.net>
2024-03-25 17:27:21 +0100

    Automatic commit

---
+++ getproxy.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import requests
+from git import Repo
+import time
+
+repo_path = '/root/grasspr/'  # Path to the Git repository
+
+while True:
+    # Download the file from the URL
+    url = 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt'
+    response = requests.get(url)
+    with open('all.txt', 'wb') as f:
+        f.write(response.content)
+
+    # Rename the file
+    os.rename('all.txt', 'proxy_list(for1).txt')
+
+    # Add all files to the Git repository
+    repo = Repo(repo_path)
+    repo.index.add('*')
+
+    # Commit changes
+    repo.index.commit('Automatic commit')
+
+    # Push changes to the 'main' branch of the remote repository
+    origin = repo.remote(name='origin')
+    origin.push('main')
+
+    # Wait for 5 minutes before running the loop again
+    time.sleep(5 * 60)
+
