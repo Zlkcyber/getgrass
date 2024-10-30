d08e4c4c3e02de70a52b936bbbb4a5785608f904
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-10-30 16:54:21 +0800

    Update getproxy.py

--- getproxy.py
+++ getproxy.py
@@ -1,32 +1,27 @@
-import subprocess
 import os
 import requests
 from git import Repo
 import time
 
-repo_path = '/root/grasspr/'  # Path to the Git repository
 
-while True:
-    # Download the file from the URL
-    url = 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt'
-    response = requests.get(url)
-    with open('all.txt', 'wb') as f:
-        f.write(response.content)
+# Download the file from the URL
+url = 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt'
+response = requests.get(url)
+with open('all.txt', 'wb') as f:
+    f.write(response.content)
 
-    # Rename the file
-    os.rename('all.txt', 'proxy.txt')
+# Rename the file
+os.rename('all.txt', 'proxy.txt')
 
-    # Add all files to the Git repository
-    repo = Repo(repo_path)
-    repo.index.add('*')
+# Initialize the Git repository in the current directory
+repo = Repo('.')
+repo.index.add(['proxy.txt'])
 
-    # Commit changes
-    repo.index.commit('Automatic commit')
+# Commit changes
+repo.index.commit('Automatic commit')
 
-    # Push changes to the 'main' branch of the remote repository
-    origin = repo.remote(name='origin')
-    origin.push('main')
+# Push changes to the 'main' branch of the remote repository
+origin = repo.remote(name='origin')
+origin.push('main')
 
-    # Wait for 5 minutes before running the loop again
-    time.sleep(5 * 60)
-
+    
