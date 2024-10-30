731a0043cc7fd3daa099ec7fa772d81cc300d83a
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-10-30 17:05:39 +0800

    Update getproxy.py

--- getproxy.py
+++ getproxy.py
@@ -5,6 +5,7 @@ import time
 
 
 # Download the file from the URL
+print("Update Proxy...")
 url = 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt'
 response = requests.get(url)
 with open('all.txt', 'wb') as f:
@@ -12,16 +13,6 @@ with open('all.txt', 'wb') as f:
 
 # Rename the file
 os.rename('all.txt', 'proxy.txt')
-
-# Initialize the Git repository in the current directory
-repo = Repo('.')
-repo.index.add(['proxy.txt'])
-
-# Commit changes
-repo.index.commit('Automatic commit')
-
-# Push changes to the 'main' branch of the remote repository
-origin = repo.remote(name='origin')
-origin.push('main')
+print("Update Proxy Success save to proxy.txt")
 
     
